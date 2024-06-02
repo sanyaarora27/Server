47d5a64807e9d5a19794a2218b9439ee317f6dd6
Sanya Arora <54741828+sanyaarora27@users.noreply.github.com>
2024-06-02 18:44:33 +0530

    Update index1.js

--- index1.js
+++ index1.js
@@ -11,90 +11,90 @@ name: "John",
 kidneys: [
 {
 healthy: false,
-},
-],
-},
-];
+}]
+}];
 
-//? checking kidenys information
-
-app.get("/", (req, res) => {
-const johnKidenys = users[0].kidneys;
-const noOfKideneys = johnKidenys.length;
-
-const healthResult = users[0].kidneys.filter(checkHealth);
-
-noOfHealthyKideneys = healthResult.length;
-
-function checkHealth(kidney) {
-return kidney.healthy ? 1 : 0;
-}
-
-const noOfUnhealthyKideneys = noOfKideneys - noOfHealthyKideneys;
-
-res.json({
-noOfKideneys,
-noOfHealthyKideneys,
-noOfUnhealthyKideneys,
-});
-});
-
-//? Adding new Kideny
-
-app.post("/add", (req, res) => {
-const isHealthy = req.body.isHealthy;
-users[0].kidneys.push({ healthy: isHealthy });
-res.json({
-msg: "New Kideny Added !",
-});
-});
-
-//? Removing all unhealthy kidenys !
-
-app.put("/replace", (req, res) => {
-const healthResult = users[0].kidneys.filter(checkHealth);
-noOfHealthyKideneys = healthResult.length;
-function checkHealth(kidney) {
-if (kidney.healthy === false) {
-kidney.healthy = true;
-}
-}
-
-res.json({ msg: "Kideny Replaced !" });
-});
-
-//? Removing all unhealthy kidenys !
-
-function isTheiratLeastOneUnhealthyKideny() {
-let atLeastOneUnhealthyKideny = false;
-for (let i = 0; i < users[0].kidneys.length; i++) {
-if (!users[0].kidneys[i].healthy) {
-atLeastOneUnhealthyKideny = true;
-}
-}
-return atLeastOneUnhealthyKideny;
-}
-
-app.delete("/remove", (req, res) => {
-if (isTheiratLeastOneUnhealthyKideny()) {
-const kidneys = users[0].kidneys;
-const healthyKideneys = [];
-
-kidneys.filter(removeUnhealthy);
-
-function removeUnhealthy(kidney) {
-  if (kidney.healthy === true) {
-    healthyKideneys.push(kidney);
-  }
-}
-users[0].kidneys = healthyKideneys;
-
-res.json({ msg: "unhealthy Kidenyes Removed !" });
-} else {
-res.status(411).json({ msg: "unhealthy Kidenyes Removed !" });
-}
-});
+//GET - the no of unhealthy kidneys
+//query parameters
+app.get("/", function (req, res) {
+    const johnKidneys = users[0].kidneys; //no of kidneys
+    // console.log(johnKidneys);
+    const noOfKidneys = johnKidneys.length;
+    let noOfHealthyKidneys = 0;
+    for (let i = 0; i < johnKidneys.length; i++) {
+    if (johnKidneys[i].healthy) {
+    noOfHealthyKidneys = noOfHealthyKidneys + 1;
+    }
+    }
+    const noOfUnhealthyKidneys = noOfKidneys - noOfHealthyKidneys;
+    res.json({
+    noOfKidneys,
+    noOfHealthyKidneys,
+    noOfUnhealthyKidneys,
+    });
+    });
+    
+    //POST - Add new Kidneys
+    //middlewares
+    app.post("/", function (req, res) {
+    const isHealthy = req.body.isHealthy;
+    users[0].kidneys.push({
+    healthy: isHealthy,
+    });
+    res.json({
+    msg: "Done!",
+    });
+    });
+    
+    //PUT - user can replace a kidney , make it healthy
+    app.put("/", function (req, res) {
+    //if there is no unhealthy kidney
+    if (isThereAtLeastOneUnhealthyKidney()) {
+    for (let i = 0; i < users[0].kidneys.length; i++) {
+    users[0].kidneys[i].healthy = true;
+    }
+    res.json({}); //tell the user that everything is done , if not the request will remain hung
+    } else {
+    res.status(411).json({
+    msg: "all kidneys are already healthy",
+    });
+    }
+    });
+    
+    //DELETE - User can remove a kidney
+    app.delete("/", function (req, res) {
+    if (isThereAtLeastOneUnhealthyKidney()) {
+    const newKidneys = [];
+    for (let i = 0; i < users[0].kidneys.length; i++) {
+    if (users[0].kidneys[i].healthy) {
+    newKidneys.push({
+    healthy: true,
+    });
+    }
+    }
+    users[0].kidneys = newKidneys;
+    res.json({
+    msg: "done",
+    });
+    } else {
+    res.status(411).json({
+    msg: "you have no unhealthy kidney",
+    });
+    }
+    });
+    
+    function isThereAtLeastOneUnhealthyKidney() {
+    //you should return a 411
+    //only if at least one unhealthy kidney is there do this , else return 411
+    let atLeastOneUnhealthyKidney = false;
+    for (let i = 0; i < users[0].kidneys.length; i++) {
+    if (!users[0].kidneys[i].healthy) {
+    atLeastOneUnhealthyKidney = true;
+    }
+    }
+    return atLeastOneUnhealthyKidney;
+    }
 
 app.listen(3000, () => {
-console.log("Server Running on 3000 !");
+console.log("Server Running on 3000.");
 });
